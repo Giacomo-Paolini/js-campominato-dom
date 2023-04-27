40758b80af5f5b0574991314217a3f3afa0607b1
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-04-27 17:07:24 +0200

    feat: html, css, js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const grid = document.getElementById("grid");
+const selection = document.getElementById("selection");
+const btn = document.getElementById("btn-play");
+
+function cellDifficulty(difficult, content) {
+    const squareDiv = document.createElement("div");
+    squareDiv.classList.add(difficult);
+    squareDiv.innerText = content
+    grid.appendChild(squareDiv);
+    squareDiv.addEventListener("click", function() {
+        squareDiv.classList.toggle("clicked");
+    });
+}
+
+btn.addEventListener("click", function() {
+    if (selection.value === "Easy") {
+        for (let i = 1; i <= 100; i++) {
+            cellDifficulty("square-easy", i);
+        }
+    } else if (selection.value === "Medium") {
+        for (let i = 1; i <= 81; i++) {
+            cellDifficulty("square-medium", i);
+        }
+    } else {
+        for (let i = 1; i <= 49; i++) {
+            cellDifficulty("square-hard", i);
+        }
+    }
+})
